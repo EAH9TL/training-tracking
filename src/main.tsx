0e1fae5266bdc69dff792058bb6b0ece059f2f57
Andre <eah9tl@gmail.com>
2025-06-28 01:12:44 -0600

    feat: basename for browser route

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { Provider } from './components/ui/provider.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <BrowserRouter>
+      <BrowserRouter basename='/training-tracking'>
         <AppRouter />
       </BrowserRouter>
     </Provider>
